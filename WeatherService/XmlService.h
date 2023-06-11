074e98b1058c640b5e16a967107f74100975af32
Ivan Botvin <ivan@gmail.com>
2023-06-11 21:01:50 +0300

    Добавлен класс XmlService

---
+++ WeatherService/XmlService.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Service.h"
+#include "Weather.h"
+#include "pugixml-1.13-20230530T065518Z-001/pugixml-1.13/src/pugixml.hpp"
+
+class XmlService :public Service
+{
+public:
+	virtual Weather getWeather(std::string s) override;
+	virtual ~XmlService() {};
+};
