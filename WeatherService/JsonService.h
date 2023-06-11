c85ad530125b285c52418b07f7f594b1c88a125d
Ivan Botvin <ivan@gmail.com>
2023-06-11 21:45:56 +0300

    Правки в файлах

--- WeatherService/JsonService.h
+++ WeatherService/JsonService.h
@@ -1,11 +1,11 @@
 #pragma once
 #include "Service.h"
-#include "Weather.h"
+#include "json.hpp"
 #include "pugixml-1.13-20230530T065518Z-001/pugixml-1.13/src/pugixml.hpp"
 
-class XmlService : public Service
+class JsonService : public Service
 {
 public:
 	virtual Weather getWeather(std::string s) override;
-	virtual ~XmlService() {};
+	virtual ~JsonService() {};
 };
